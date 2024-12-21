81f946556d43ddfcd690d46f5169bd4a4dcf7e7d
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2024-12-21 22:56:46 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 180 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        short y = in.nextShort();
+        System.out.println(y - 543);
+    }
+}
