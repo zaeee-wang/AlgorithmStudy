48e9bbffc10b1bb43491a0f25e7e2b478df9b0e1
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2024-12-21 22:51:28 +0900

    [Bronze V] Title: A+B - 4, Time: 188 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextShort()){
+            short a = in.nextShort();
+            short b = in.nextShort();
+            System.out.println(a+b);
+        }
+    }    
+}
