1589342010a5163f923cf31a39775d48b81aadf9
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2024-12-21 22:52:21 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int n = a / 4;
+        for(int i = 0; i < n; i++){
+            System.out.print("long ");
+        }
+        System.out.print("int");
+    }    
+}
