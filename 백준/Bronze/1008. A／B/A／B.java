50dd7e8683a4ed9bec7429a1ce83af1185087736
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2024-12-21 22:50:07 +0900

    [Bronze V] Title: A/B, Time: 188 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double a,b;
+        Scanner in = new Scanner(System.in);
+        a = in.nextDouble();
+        b = in.nextDouble();
+        System.out.println(a/b);
+    }
+}
