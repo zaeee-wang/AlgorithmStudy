bcd99402e62013c53b8df3409561759e2722a3e1
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2024-12-21 22:51:49 +0900

    [Bronze V] Title: A+B - 8, Time: 228 ms, Memory: 18964 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        short t = in.nextShort();
+        for(int i = 0; i < t; i++){
+            short a = in.nextShort();
+            short b = in.nextShort();
+            System.out.println(("Case #"+(i+1)+": "+a+" + "+ b + " = " +(a+b)));
+        }
+    }   
+}
