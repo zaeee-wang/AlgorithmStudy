31e99a51b4d45c9807a202ce3a32e57e58c84618
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2024-12-23 22:46:05 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 168 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {         
         Scanner in = new Scanner(System.in);
-        String str = in.next();
-        char[] word = new char[str.length()];
-        for(int i = 0; i < str.length(); i++){
-            word[i] = str.charAt(str.length()-i-1);
-        }
-        if(str.equals(String.valueOf(word)))System.out.println(1);
+        String str = in.next();        
+        // 문자열 reverse        
+        StringBuffer sb = new StringBuffer(str);        
+        String reverse = sb.reverse().toString();         
+        if(str.equals(reverse))System.out.println(1);
         else System.out.println(0);
-    }
+        }
 }
