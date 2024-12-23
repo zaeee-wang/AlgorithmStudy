215a2247f26b282ce9c126d09a0c416956ca64fc
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2024-12-23 12:25:19 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "         ,r'\"7\n" + 
+                            "r`-_   ,'  ,/\n" + 
+                            " \\. \". L_r'\n" + 
+                            "   `~\\/\n" + 
+                            "      |\n" + 
+                            "      |";
+        System.out.print(str);
+    }
+}
