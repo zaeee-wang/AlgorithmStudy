92c1f8fe359ea204370b4fc98a1c7fe931f2edda
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2025-07-01 17:21:07 +0900

    [Bronze II] Title: 팰린드롬, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/13235. 팰린드롬/팰린드롬.java
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.java
@@ -4,11 +4,19 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static boolean IsPalindrome(String s){
-        StringBuilder sb = new StringBuilder(s);
-        String reverse = sb.reverse().toString();
-        for (int i = 0; i < reverse.length(); i++) {
-            if(s.charAt(i)!=reverse.charAt(i)){
-                return false;
+        int start = 0;
+        int end = s.length()-1;
+        while (start < end) {
+            if(!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            } else if(!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            } else{
+                if(Character.toLowerCase(s.charAt(start))!=Character.toLowerCase(s.charAt(end))){
+                    return false;
+                }
+                start++;
+                end--;
             }
         }
         return true;
