5ea816d536e381b83ee45f90cf6da679c4802b8c
Lee Jae-wang <98081812+zaeee-wang@users.noreply.github.com>
2025-07-22 15:36:46 +0900

    [Silver IV] Title: 스택 2, Time: 1064 ms, Memory: 241148 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+
+            switch (cmd) {
+                case 1:
+                    int option = Integer.parseInt(st.nextToken());
+                    stack.push(option);
+                    break;
+
+                case 2:
+                    if (stack.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(stack.pop() + "\n");
+                    }
+                    break;
+                case 3:
+                    bw.write(stack.size() + "\n");
+                    break;
+                case 4:
+                    if (stack.isEmpty()) {
+                        bw.write("1\n");
+                    } else {
+                        bw.write("0\n");
+                    }
+                    break;
+                case 5:
+                    if (stack.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(stack.peek() + "\n");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
